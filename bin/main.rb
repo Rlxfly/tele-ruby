3b372ad80de86a63970934e37e520d9f960798cb
Rell <me@rlxfly.my.id>
2022-09-15 17:00:48 +0700

    Create main.rb

---
+++ bin/main.rb
@@ -0,0 +1,14 @@
+require_relative '../lib/bot.rb'
+require_relative '../lib/motivate.rb'
+
+puts "Hello! This is beta Telegram bot by Re7~"
+puts "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
+puts "This is a bot to help you stay motived by giving you motivational quotes"
+puts "This bot can also keep you entertained by providing you with jokes"
+puts "This bot will help you when you feeling down and demotived"
+sleep(0.5)
+puts "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
+puts "Loading..."
+puts "The bot is now online!"
+puts "Use CTRL + C to stop the bot~"
+Bot.new
